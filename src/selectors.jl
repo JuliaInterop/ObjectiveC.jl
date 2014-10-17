f47d0da7bcc15368cc70c70061ae9903d00c14e2
Mike Innes <mike.j.innes@gmail.com>
2014-10-17 11:40:51 +0100

    remove selector parametrisation for now

--- src/selectors.jl
+++ src/selectors.jl
@@ -2,18 +2,18 @@ selname(s::Ptr{Void}) =
   ccall(:sel_getName, Ptr{Cchar}, (Ptr{Void},),
         s) |> bytestring
 
-immutable Selector{name}
+immutable Selector#{name}
   ptr::Ptr{Void}
   Selector(ptr::Ptr{Void}) = new(ptr)
 end
 
-Selector(name::Symbol, ptr::Ptr{Void}) = Selector{name}(ptr)
-Selector(ptr::Ptr{Void}) = Selector(symbol(selname(ptr)), ptr)
+#Selector(name::Symbol, ptr::Ptr{Void}) = Selector{name}(ptr)
+#Selector(ptr::Ptr{Void}) = Selector(symbol(selname(ptr)), ptr)
 
 convert(::Type{Ptr{Void}}, sel::Selector) = sel.ptr
 
 function Selector(name)
-  Selector(symbol(name),
+  Selector(#symbol(name),
            ccall(:sel_registerName, Ptr{Void}, (Ptr{Cchar},),
                  string(name)))
 end
@@ -22,7 +22,7 @@ macro sel_str(name)
   Selector(name)
 end
 
-name{T}(sel::Selector{T}) = T
+name(sel::Selector) = selname(sel.ptr)
 
 function show(io::IO, sel::Selector)
   print(io, "sel")
