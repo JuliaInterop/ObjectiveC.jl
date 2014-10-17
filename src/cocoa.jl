c2be16efa2ac4cb2af4a6eb1ca1002b196127a6b
Mike Innes <mike.j.innes@gmail.com>
2014-10-17 17:07:19 +0100

    add cocoa module

--- src/cocoa.jl
+++ src/cocoa.jl
@@ -1,5 +1,26 @@
+module Cocoa
+
+using ObjectiveC
+
 dlopen("/System/Library/Frameworks/AppKit.framework/Versions/C/Resources/BridgeSupport/AppKit")
 
-for c in :[NSWindow NSAlert].args
-  @eval const $c = Class($(Expr(:quote, c)))
+for c in :[NSAlert
+           NSWindow
+           NSColor].args
+  @eval $(Expr(:export, c))
+  @eval const $c = ObjectiveC.Class($(Expr(:quote, c)))
+end
+
+export alert
+
+function alert(header, body)
+  @objc begin
+    msg = [[NSAlert alloc] init]
+    [msg setMessageText: header]
+    [msg setInformativeText: body]
+    [msg runModal]
+    [msg release]
+  end
+end
+
 end
