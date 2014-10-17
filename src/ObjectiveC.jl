ead2bf58049cdd678a23b3a79536d7e977a9492b
Mike Innes <mike.j.innes@gmail.com>
2014-10-17 17:11:24 +0100

    clean up main file

--- src/ObjectiveC.jl
+++ src/ObjectiveC.jl
@@ -19,26 +19,8 @@ include("call.jl")
 include("syntax.jl")
 
 # API wrappers
-include("cocoa.jl")
 include("foundation.jl")
 
-# Import some classes
-for c in :[NSObject NSString NSArray].args
-  @eval const $c = Class($(Expr(:quote, c)))
 end
 
-# @objc [NSString new]
-
-# calltransform(:[NSString foo:"foo" bar:2])
-
-# methods(NSString)
-
-# @objc [[NSWindow alloc] init]
-
-# @objc begin
-#   alert = [[NSAlert alloc] init]
-#   [alert runModal]
-#   [alert release]
-# end
-
-end
+include("cocoa.jl")
