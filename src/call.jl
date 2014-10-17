c84c083c08f13d6a3e10d7d7c46bba3b98d8206c
Mike Innes <mike.j.innes@gmail.com>
2014-10-17 14:27:30 +0100

    implement general method calls

--- src/call.jl
+++ src/call.jl
@@ -1,3 +1,8 @@
+# ccal
+# This is basically a horrible hack around not being able to call C
+# varargs functions. Emulates `ccall` but as a function taking a function
+# pointer + types, args etc.
+
 Base.eltype{T}(::Type{Type{T}}) = T
 
 stagedfunction ccal(f::Ptr, t, ts, vals...)
@@ -10,4 +15,25 @@ stagedfunction ccal(f::Ptr, t, ts, vals...)
       ccall(f, $R, $AS, $(args...))
     end
   end
+end
+
+# Calls
+
+ctype(x) = x
+ctype(o::Type{Object}) = Ptr{Void}
+ctype(s::Type{Selector}) = Ptr{Void}
+ctype(a::AbstractArray) = map(ctype, a)
+
+const cmsgsend = cglobal(:objc_msgSend)
+
+function message(obj, sel, args...)
+  clarse = class(obj)
+  m = method(clarse, sel)
+  m == C_NULL && error("$(clarse) does not respond to $sel")
+  types = signature(m)
+  ctypes = ctype(types)
+  result = ccal(cmsgsend, ctypes[1], tuple(ctypes[2:end]...),
+                obj, sel, args...)
+  types[1] in (Object, Selector) && return types[1](result)
+  return result
 end
