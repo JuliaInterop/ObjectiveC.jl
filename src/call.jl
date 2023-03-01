f3a68dc505930026f669109073f403970c088b4f
Tim Besard <tim.besard@gmail.com>
2023-03-01 09:51:34 +0100

    Use a generated function, not eval, to call objc_msgSend with varargs.

--- src/call.jl
+++ src/call.jl
@@ -1,26 +1,36 @@
-# ccal
-# This is basically a horrible hack around not being able to call C
-# varargs functions. Emulates `ccall` but as a function taking a function
-# pointer + types, args etc.
+# varargs_ccall
 
-Base.eltype(::Type{Type{T}}) where {T} = T
+# variant of ccall that supports varargs
 
-# stagedfunction ccal(f::Ptr, t, ts, vals...)
-#   R = eltype(t)
-#   AS = Expr(:tuple, eltype(ts)...)
-#   args = map(x->gensym(), vals)
-#   quote
-#     let
-#       $(Expr(:tuple, args...)) = vals
-#       ccall(f, $R, $AS, $(args...))
-#     end
-#   end
-# end
+@generated function varargs_ccall(f::Ptr, _rettyp, _types, vals...)
+    ex = quote end
 
-function ccal(f, R, ts, vals...)
-  AS = Expr(:tuple, ts...)
-  @eval ccall($f, $R, $AS, $(vals...))
+    rettyp = _rettyp.parameters[1]
+    types = _types.parameters[1].parameters
+    args = [:(vals[$i]) for i in 1:length(vals)]
+
+    # cconvert
+    cconverted = [Symbol("cconverted_$i") for i in 1:length(vals)]
+    for (dst, typ, src) in zip(cconverted, types, args)
+      append!(ex.args, (quote
+         $dst = Base.cconvert($typ, $src)
+      end).args)
+    end
+
+    # unsafe_convert
+    unsafe_converted = [Symbol("unsafe_converted_$i") for i in 1:length(vals)]
+    for (dst, typ, src) in zip(unsafe_converted, types, cconverted)
+      append!(ex.args, (quote
+         $dst = Base.unsafe_convert($typ, $src)
+      end).args)
+    end
+
+    call = Expr(:foreigncall, :f, rettyp, Core.svec(types...), 0, QuoteNode(:ccall), unsafe_converted..., cconverted...)
+    push!(ex.args, call)
+
+    return ex
 end
+
 
 # Calls
 
@@ -36,6 +46,7 @@ ctype(a::AbstractArray) = map(ctype, a)
 const cmsgsend = cglobal(:objc_msgSend)
 
 function message(obj, sel, args...)
+  # FIXME: can we do these look-ups at compile time so that we can use `@ccall`?
   obj = toobject(obj)
   clarse = class(obj)
   m = method(clarse, sel)
@@ -48,8 +59,8 @@ function message(obj, sel, args...)
     types[i+3] == Object && (args[i] = toobject(args[i]))
   end
 
-  result = ccal(cmsgsend, ctypes[1], tuple(ctypes[2:end]...),
-                obj, sel, args...)
+  result = varargs_ccall(cmsgsend, ctypes[1], Tuple{ctypes[2:end]...},
+                         obj, sel, args...)
   types[1] in (Object, Selector) && return types[1](result)
   return result
 end
