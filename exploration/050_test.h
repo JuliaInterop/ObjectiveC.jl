b9d78f9f06f3c2ba43e0f8a859bf6555671d2b8e
Nehal Patel <nehal@alum.mit.edu>
2023-03-14 12:28:26 -0400

    explore modulemaps, apinotes, objc+clang.jl

---
+++ exploration/050_test.h
@@ -0,0 +1,17 @@
+
+typedef struct {int a;int b;} Bar;
+
+int m1(Bar in){return in.a*in.b;           }
+Bar m2(Bar in){in.a*=2; in.b*=2; return in;}
+
+@interface Foo 
+- (int) m0:(int) x;
+- (int) m1:(Bar) x;
+- (Bar) m2:(Bar) x;
+@end
+@implementation Foo {}
+- (int) m0:(int) x {return 2*x              ;}
+- (int) m1:(Bar) x {return x.a*x.b          ;}
+- (Bar) m2:(Bar) x {x.a*=2; x.b*=2; return x;}
+@end
+
